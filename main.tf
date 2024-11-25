a072ccaa5aa57c5b23c3aef4bdc329db090dbb22
avishkar <avishkaravhad87@gmail.com>
2024-11-25 11:45:48 +0000

    GCP-Terraform

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "google_service_account" "default" {
+  account_id   = "a18aa1-f873cf-a45b19"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "free-tier-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project     = "flowing-access-433413-d5"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
